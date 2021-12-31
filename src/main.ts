2f5d51768f4a85d32c9f364adb3219e956318752
hefeixiong <1074535830@qq.com>
2021-12-31 21:01:55 +0800

    element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+//element-plus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+app.use(ElementPlus);
+
+createApp(App).mount('#app');
